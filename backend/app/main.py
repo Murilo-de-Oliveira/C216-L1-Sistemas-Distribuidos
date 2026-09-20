fa25f863f36fdc3b2b3f8662535610e5e95822e5
Murilo de Oliveira <murilo.o.d.f@gmail.com>
2026-09-19 23:21:09 -0300

    chore: Applying lint changes

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,11 +2,12 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+
 @app.get("/")
 def home():
     return {"message": "Olá, Sistemas Distribuídos"}
 
+
 @app.get("/hello/{name}")
 def hello(name: str):
     return {"message": f"Olá, {name}"}
-
